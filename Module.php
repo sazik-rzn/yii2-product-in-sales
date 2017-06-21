87cad684da27f7076c41cba6f09f9fc4f70e8ce8
Vladimir Sazonov <sazonov@rumex.ru>
2017-06-21 09:14:37 +0300

    Params preload

--- Module.php
+++ Module.php
@@ -14,6 +14,8 @@ class Module extends \yii\base\Module {
      * @var string|boolean 
      */
     public $offer_model_class = false;
+    public $params_preload_method_name = false;
+    public $params_model_class = false;
 
     const params_path = 'product_in_sales';
     const offer_model_class_param = 'offer_class';
@@ -27,6 +29,11 @@ class Module extends \yii\base\Module {
     }
 
     private function initParams() {
+        if($this->params_model_class && $this->params_preload_method_name){
+            $class = $this->params_model_class;
+            $func = $this->params_preload_method_name;
+            $class::$func();
+        }
         \Yii::$app->params[self::params_path] = [];
     }
 
